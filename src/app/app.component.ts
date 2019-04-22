3cd677acd6448a7cbda241ef9899f65ac278b870
manikanta <manikanta@pratian.com>
2019-04-22 19:22:47 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import Typed from 'typed.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myApp';
+  title = 'Manikanta Nandam';
+
+  //  options = {
+  //   strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
+  //   typeSpeed: 40
+  // };
+
+  //  typed = new Typed(".typed-strings", this.options);
 }
